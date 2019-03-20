65bfb3137eca2bf418fa8d6a2a08f972fa8e3db1
Ryan Daly <ryan.w.daly90@gmail.com>
2019-03-20 17:08:34 -0400

    up and running

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
- DNA = require("DNA")
- population = require("Population")
+
+Population = require("./Population");
+
+const target = "This is an example of a genetic algorithm.";
+const popMax = 400;
+const mutationRate = 0.001;
+
+// var bestPhrase;
+// var allPhrases;
+// var stats;
+var population;
+
+population = new Population(target, mutationRate, popMax)
+
+while(!population.isTargetMatch()) {
+    population.calcFitness();
+    population.naturalSelection();
+    population.generate();
+}
+
+console.log("target matched!");
+console.log("Total Generations:", population.totalGenerations)
+
+
+
+
+
 
